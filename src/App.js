ee007f49ec43ce165b514a91952d1142b3ddb0bd
webthemelab <75390272+webthemelab@users.noreply.github.com>
2021-03-03 18:54:47 +0600

    axios use api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import News from './components/News/News';
 import Header from './components/Header/Header'
+import axios from 'axios';
 
 function App() {
   const [articles, setArticles] = useState([]);
@@ -9,8 +10,14 @@ function App() {
     const url ='http://newsapi.org/v2/everything?q=tesla&from=2021-02-03&sortBy=publishedAt&apiKey=9317483517b043d38ed61b68149f8c13'
     fetch(url)
     .then(res => res.json())
-    .then(data => setArticles(data.articles))
+    .then(data => console.log(data))
   },[])
+
+  useEffect( () => {
+    const url ='http://newsapi.org/v2/everything?q=tesla&from=2021-02-03&sortBy=publishedAt&apiKey=9317483517b043d38ed61b68149f8c13'
+    axios(url)
+    .then(data => setArticles(data.data.articles))
+  }, [])
   return (
     <div>
         <Header></Header>
