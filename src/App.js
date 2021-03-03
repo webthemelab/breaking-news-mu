383e9d647907a76b175eb2a6fd443362c39d8978
webthemelab <75390272+webthemelab@users.noreply.github.com>
2021-03-03 17:45:03 +0600

    finih=shed

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import News from './components/News/News';
+import Header from './components/Header/Header'
+
+function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() =>{
+    const url ='http://newsapi.org/v2/everything?q=tesla&from=2021-02-03&sortBy=publishedAt&apiKey=9317483517b043d38ed61b68149f8c13'
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setArticles(data.articles))
+  },[])
+  return (
+    <div>
+        <Header></Header>
+        <h2>Article:{articles.length}</h2>
+        {
+          articles.map(article => <News article = {article} ></News>)
+        }
+         
+    </div>
+  );
+}
+
+export default App;
